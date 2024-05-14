59e4e71b0bd33eb792b46128923ddb53eed62f77
Gabrielle Cavichion <102532618+gbrllcavichion@users.noreply.github.com>
2024-05-14 16:56:05 -0300

    :sparkles: PrimeNg Config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stock-control';
+
+  constructior(private primeNgConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
+  }
+  
 }
